9f26907bf1d178c84b7c3164e78e397f6edd5b3c
Molid <delmolid@hotmail.fr>
2024-11-15 16:05:14 +0100

    realisation etape4

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const latinToMorse = {
 	'Z':'--..'
 }
 function getLatinCharacterList (latin){
-  return latin.split('')
+  return latin.split('');
 }
 // console.log(getLatinCharacterList("Hello, world"))
 
@@ -48,4 +48,59 @@ const encodeLatin = getLatinCharacterList (latin);
   return encodemorse
 }
 console.log(encode("HELLO,WORLD", latinToMorse));
+
+const morseToLatin = {
+    '-': "T",
+    '--': "M",
+    '---': "O",
+    '--.': "G",
+    '--.-': "Q",
+    '--..': "Z",
+    '-.': "N",
+    '-.-': "K",
+    '-.--': "Y",
+    '-.-.': "C",
+    '-..': "D",
+    '-..-': "X",
+    '-...': "B",
+    '.': "E",
+    '.-': "A",
+    '.--': "W",
+    '.---': "J",
+    '.--.': "P",
+    '.-.': "R",
+    '.-..': "L",
+    '..': "I",
+    '..-': "U",
+    '..-.': "F",
+    '...': "S",
+    '...-': "V",
+    '....': "H"
+  }
+
+  /* 
+  Vous trouverez en annexe 2 le dictionnaire de correspondance inversé. 
+  Ajoutez-le à votre code, 
+  et appliquez les procédés similaire à ce qui a été fait pour le encode pour créer une fonction decode. 
+  Dans cet exercice, on admettra que les lettres en morse sont séparées par un espace, et les mots par des “/” (slash).
+  Ainsi, créer la fonction getMorseCharacterList ainsi que translateMorseCharacter.
+ */
+
+
+function getMorseCharacterList (morse){
+    return morse.split('');
+  }
+  console.log(getMorseCharacterList('-..'));
+
+  function translateMorseCharacter (latin, morseToLatin){
+    return morseToLatin[latin]
+  }
+  console.log(translateMorseCharacter("-..", morseToLatin));
+
+function decode (morse,morseToLatin){
+    const decodeLatin = getMorseCharacterList (morse);
+     let decodemorse= decodeLatin.map(char => morseToLatin[char])
+      return decodemorse
+    }
+    console.log(decode('.', morseToLatin));
 
